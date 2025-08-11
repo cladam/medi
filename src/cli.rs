82bd4ff03c021f31ed057284cd6e4340b7d38b23
Claes Adamsson <claes.adamsson@ingka.ikea.com>
2025-08-11 11:05:35 +0200

    fix: Add helping comment on the CLI struct, will show in --help flag

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,14 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(
+    name = "medi",
+    author = "Claes Adamsson @cladam",
+    version,
+    about = "A CLI tool for Trunk-Based Development (TBD) workflows",
+    long_about = None)]
+#[command(propagate_version = true)]
+
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
@@ -9,11 +16,18 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
+    /// Create a new note with the specified key.
     New { key: String },
+    /// Edit an existing note with the specified key.
     Edit { key: String },
+    /// Get the content of a note with the specified key.
     Get { key: String },
+    /// List all notes.
     List,
+    /// Delete a note with the specified key.
     Delete { key: String },
+    /// Import notes from a file.
     Import { file: String },
+    /// Export notes to a file.
     Export { file: String },
 }
